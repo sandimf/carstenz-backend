4d15f0ffcb7bfc05273d7cdfbcb12eab5e82de31
sandimf <sndimf@gmail.com>
2025-10-09 14:00:59 +0700

    add csv export

--- app/Http/Controllers/CarstenzController.php
+++ app/Http/Controllers/CarstenzController.php
@@ -305,12 +305,5 @@ private function isJson($string) {
 }
 
 
-private function isJson($string) {
-    if (!is_string($string)) return false;
-    json_decode($string);
-    return json_last_error() === JSON_ERROR_NONE;
-}
-
-
 }
 
